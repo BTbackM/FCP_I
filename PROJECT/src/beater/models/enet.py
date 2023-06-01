4b65f86a105b040c1127b15ad75157eee2126bbf
BT <felix.blanco@utec.edu.pe>
2023-06-01 12:39:00 -0500

    feat: Bottleneck module of ENet

--- PROJECT/src/beater/models/enet.py
+++ PROJECT/src/beater/models/enet.py
@@ -11,7 +11,7 @@ class InitialBlock(nn.Module):
                  in_channels,
                  out_channels,
                  ) -> None:
-        super().__init__()
+        super(InitialBlock, self).__init__()
 
         # Main branch (left side): performs regular convolution
         self.main_branch = nn.Conv2d(in_channels = in_channels,
@@ -40,3 +40,72 @@ class InitialBlock(nn.Module):
         out = self.activation(out)
 
         return out
+
+class Bottleneck(nn.Module):
+    """
+    Based on paper: https://arxiv.org/pdf/1606.02147.pdf
+    Bottleneck module of the model ENet
+    """
+    def __init__(self,
+                 in_channels,
+                 out_channels,
+                 dilated = False,
+                 asymmetric = False,
+                 downsample = False,
+                 upsample = False,
+                 p = 0.01,
+                 ) -> None:
+        super(Bottleneck, self).__init__()
+        self.downsample = downsample
+        self.upsample = upsample
+        # Dilated bottleneck params
+        if dilated:
+            dilation = 2
+            padding = 2
+            stride = 1
+        # Asymmetric bottleneck params
+        elif asymmetric:
+            dilation = 1
+            padding = 4
+            stride = 1
+        # Upsample bottleneck params
+        elif upsample:
+            dilation = 1
+            padding = 0
+            stride = 1
+        # Downsample bottleneck params
+        else:
+            dilation = 1
+            padding = 1
+            stride = 2
+
+        # Conv 1x1 projection layer
+        self.bottleneck_projection = nn.Sequential(
+            nn.Conv2d(in_channels = in_channels,
+                      out_channels = out_channels,
+                      kernel_size = 1,
+                      stride = 1,
+                      padding = 0,),
+            nn.BatchNorm2d(out_channels),)
+
+        # Main conv layer
+        self.bottleneck_main = nn.Sequential(
+            nn.Conv2d(in_channels = out_channels,
+                      out_channels = out_channels,
+                      kernel_size = 3,
+                      stride = stride,
+                      padding = padding,
+                      dilation = dilation,),
+            nn.BatchNorm2d(out_channels),)
+
+        # Conv 1x1 expansion layer
+        self.bottleneck_expansion = nn.Sequential(
+            nn.Conv2d(in_channels = out_channels,
+                      out_channels = out_channels,
+                      kernel_size = 1,
+                      stride = 1,
+                      padding = 0,),
+            nn.BatchNorm2d(out_channels),)
+
+        # Regularizer layer: dropout
+        self.dropout = nn.Dropout2d(p = p)
